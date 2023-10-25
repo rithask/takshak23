1925425154237a5c483244be32fd5fa7d2c84eee
Rithas K <rithask@icloud.com>
2023-10-25 23:04:34 +0530

    Set up react for proshows container

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles.css'
 import App from './App'
+import Proshows from './Proshows'
 
 ReactDOM.render(<App />, document.getElementById('hero-container'))
+
+const rootElement = document.getElementById('proshows-container');
+ReactDOM.render(
+  <React.StrictMode>
+    <Proshows />
+  </React.StrictMode>,
+  rootElement
+);

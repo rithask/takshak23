e4fcda89c6074bf3c879100f508b95315196d7be
Rithas K <rithask@icloud.com>
2023-10-25 21:48:09 +0530

    Add hero section and about section

---
+++ public/script.js
@@ -0,0 +1,23 @@
+// let lastScrollTop = 0;
+// const nav = document.querySelector(".nav");
+
+// // Initially set the navbar to be visible
+// nav.classList.add("active");
+
+// window.addEventListener("scroll", () => {
+// 	const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+// 	if (scrollTop > lastScrollTop) {
+// 		// Scrolling down
+// 		if (scrollTop > 100) {
+// 			nav.classList.remove("active");
+// 		} else {
+// 			nav.classList.add("active");
+// 		}
+// 	} else if (scrollTop < lastScrollTop) {
+// 		// Scrolling up
+// 		nav.classList.add("active");
+// 	}
+
+// 	lastScrollTop = scrollTop;
+// });

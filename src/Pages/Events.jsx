fa06c45a03f667eafa89981eb7f12b06eb8875c6
Rithas K <rithask@icloud.com>
2023-11-09 12:41:32 +0530

    Add events to events section

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import cinescope from '../Images/cinescope.webp';
+import codecrusade from '../Images/codecrusade.webp';
 import cons from '../Images/cons.webp';
 import debate from '../Images/debate.webp';
-import tugOfWar from '../Images/robo_tug_of_war.webp';
+import designathon from '../Images/designathon.webp';
+import nouvam from '../Images/nouvam.webp';
+import robotugOfWar from '../Images/robo_tug_of_war.webp';
+import techbiz from '../Images/techbiz.webp';
+import techquest from '../Images/techquest.webp';
+import untangle from '../Images/untangle.webp';
+import weaponexpo from '../Images/weaponexpo.webp';
 
 const Events = () => {
   const cities = [
-    { name: '', imageUrl: cons, num: '01' },
-	{ name: '', imageUrl: debate, num: '02' },
-	{ name: '', imageUrl: tugOfWar, num: '03' },
+    { name: '', imageUrl: codecrusade, num: '01' },
+	{ name: '', imageUrl: techquest, num: '02' },
+	{ name: '', imageUrl: designathon, num: '03' },
+	{ name: '', imageUrl: nouvam, num: '04' },
+	{ name: '', imageUrl: robotugOfWar, num: '05' },
+	{ name: '', imageUrl: cons, num: '06' },
+	{ name: '', imageUrl: debate, num: '07' },
+	{ name: '', imageUrl: techbiz, num: '08' },
+	{ name: '', imageUrl: untangle, num: '09' },
+	{ name: '', imageUrl: weaponexpo, num: '10' },
+	{ name: '', imageUrl: cinescope, num: '11' },
   ];
 
   return (

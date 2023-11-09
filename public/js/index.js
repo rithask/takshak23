a221f17775e91ecece66fb2af8bc8df1f43f5b6a
Rithas K <rithask@icloud.com>
2023-11-09 10:04:06 +0530

    Uncomment navbar hiding

--- public/js/index.js
+++ public/js/index.js
@@ -133,20 +133,20 @@ const nav = document.querySelector(".nav");
 // Initially set the navbar to be visible
 nav.classList.add("active");
 
-// window.addEventListener("scroll", () => {
-// 	const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener("scroll", () => {
+	const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-// 	if (scrollTop > lastScrollTop) {
-// 		// Scrolling down
-// 		if (scrollTop > 100) {
-// 			nav.classList.remove("active");
-// 		} else {
-// 			nav.classList.add("active");
-// 		}
-// 	} else if (scrollTop < lastScrollTop) {
-// 		// Scrolling up
-// 		nav.classList.add("active");
-// 	}
+	if (scrollTop > lastScrollTop) {
+		// Scrolling down
+		if (scrollTop > 100) {
+			nav.classList.remove("active");
+		} else {
+			nav.classList.add("active");
+		}
+	} else if (scrollTop < lastScrollTop) {
+		// Scrolling up
+		nav.classList.add("active");
+	}
 
-// 	lastScrollTop = scrollTop;
-// });
+	lastScrollTop = scrollTop;
+});
